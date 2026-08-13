10ed842a6c30bf2f27090274756c8e2e0d31f8a3
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2026-08-14 00:03:19 +0800

    rand函数的本质

--- basic/rand_test.go
+++ basic/rand_test.go
@@ -21,6 +21,8 @@ func BenchmarkRandV2(b *testing.B) {
 	}
 }
 
+// 本质还是下面的函数的逻辑，
+// 用了randv2这个包下的intN这个函数，rander变成全局的，每次都不一样，和时间戳有关
 func TestRand(t *testing.T) {
 	for i := 0; i < 5; i++ {
 		fmt.Printf("%d  ", randv2.IntN(100))
@@ -28,11 +30,15 @@ func TestRand(t *testing.T) {
 }
 
 func TestRandSeed(t *testing.T) {
+	// 初始种子都是123456，所以这个结果肯定是固定的
 	source := randv2.NewPCG(123, 456)
 	for i := 0; i < 5; i++ {
 		// source.Seed(123, 456)
+		// 如果把上面注释打开，那每次随机数都一样了
 		rander := randv2.New(source)
 		fmt.Printf("%d  ", rander.IntN(100))
+		//每次生成随机数的时候会有一个新的种子，这个种子会作用到新的source里面去，
+		//所以每次生成随机数都是一个新的种子在这个循环里
 	}
 }
 
@@ -40,7 +46,13 @@ func TestRandSeed(t *testing.T) {
 // go test -v ./basic -run=^TestRandSeed$ -count=1
 // go test ./basic -bench=Rand -run=^$ -count=1
 
-/*
-BenchmarkRand-8         100000000               11.43 ns/op
-BenchmarkRandV2-8       182933689                6.206 ns/op
-*/
+// PS E:\Project\go_project\go_basic\basic> go test . -bench=Rand -run=^$ -count=1
+// goos: windows
+// goarch: amd64
+// pkg: shy/go_basic/basic
+// cpu: AMD Ryzen 7 4800H with Radeon Graphics
+// BenchmarkRand-16        84267295                15.11 ns/op
+// BenchmarkRandV2-16      147941905                7.794 ns/op
+// PASS
+// ok      shy/go_basic/basic      4.024s
+// PS E:\Project\go_project\go_basic\basic>
