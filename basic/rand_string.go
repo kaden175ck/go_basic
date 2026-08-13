495b7ee9fb4dfb17c0c19f9fc9295e96a6f80e33
shihaoyang.ykuj <shihaoyang.ykuj@bytedance.com>
2026-08-13 11:39:34 +0800

    增加注释

--- basic/rand_string.go
+++ basic/rand_string.go
@@ -6,9 +6,11 @@ import (
 	"strings"
 )
 
-var letterCollection = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY/_+大乔中国")
+var letterCollection = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/_+石头")
 
 // 生成长度为n的随机字符串
+// 注释掉的是使用切片的实现，理解简单，但转 string 时会多一次内存拷贝
+// strings.Builder （当前用的）：更推荐，性能更好，内存分配更少
 func RandString(n int) string {
 	// rect := make([]rune, 0, n)
 	sb := strings.Builder{}
